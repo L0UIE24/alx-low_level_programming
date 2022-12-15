452751cf825dfcd5db6b1aef02752e626f383a89
L0UIE24 <luyandaangcobo@gmail.com>
2022-12-15 06:50:32 -0800

    parallel uni

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -3,19 +3,22 @@
 /**
  * print_diagonal - draws a diagonal line on the terminal
  * @n: number of times the character \ should be printed
- * Return: ways 0.
+ * Return: Always 0.
  */
+
 void print_diagonal(int n)
 {
 	int i, j;
 
-	for  (i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
-		for (j = 0; j < 1; j++)
+		for (j = 0; j < n; j++)
 		{
 			_putchar(' ');
 		}
-		_putchar(92);
+
+		_puthar(92);
+
 		if (i < (n - 1))
 			_putchar('\n');
 	}
