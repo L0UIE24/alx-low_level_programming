3854042e76fb2ced2094563522850717d6802ac0
L0UIE24 <luyandaangcobo@gmail.com>
2022-12-22 13:48:44 -0800

    task 6

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * leet - function that encode a string
+ * @str: string that will be encoded
+ * Return: returns encoded string
+ */
+
+char *leet(char *);
+{
+	int index1 = 0, index2;
+	char leet[8] = {'o', 'L'
